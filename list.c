44ef3ebd2cb12afe804d04ac6944e31ffc7ee358
Baudouin Feildel <b.feildel@gmail.com>
2014-12-03 16:52:21 +0100

    Update Array to list

--- list.c
+++ list.c
@@ -1,4 +1,4 @@
-#include "array.h"
+#include "list.h"
 
 // Compare functions
 int Int_cmp(int n1, int n2)
@@ -19,6 +19,11 @@ int Double_cmp(double n1, double n2)
 int Ptr_cmp(void * pt1, void * pt2)
 {
 	return pt1 - pt2;
+}
+
+int Str_cmp(char * str1, char * str2)
+{
+	return strcmp(str1, str2);
 }
 
 // Copy functions
@@ -42,7 +47,18 @@ void Ptr_copy(void ** dest, void ** src)
 	*dest = *src;
 }
 
+void Str_copy(char ** dest, char ** src)
+{
+	int i = 0;
+	int len = strlen(*src);
+	*dest = malloc(sizeof(char) * len);
+	for(i = 0 ; i < len ; i++)
+		(*dest)[i] = (*src)[i];
+	(*dest)[len] = '\0';
+}
+
 // Free functions
 void Int_free(int val)       { (void)(val); }
 void Float_free(float val)   { (void)(val); }
 void Double_free(double val) { (void)(val); }
+void Str_free(char * str)    { free(str);   }
